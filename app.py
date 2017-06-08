3ef761f4cc8ba130988e6bc1407626e6a952afae
slawiko <shchaurouski.slava@gmail.com>
2017-06-09 01:06:10 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+
+
+from telegram.ext import Updater, CommandHandler
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
+# TODO: remove this token
+updater = Updater('383721901:AAG2BFw0AasmDuB4hP943pAYNp9ZV04fprI')
+dispatcher = updater.dispatcher
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
+
